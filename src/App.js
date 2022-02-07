8f9b18619f63528ebe782f284e7b20fa59dc0a8c
Angel <angel.aven.c@gmail.com>
2022-02-06 18:43:43 -0600

    State management
    - Connecting components information

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'
+import {TodoCounter} from './TodoCounter'
+import {TodoSearch} from './TodoSearch'
+import {TodoList} from './TodoList'
+import {TodoItem} from './TodoItem'
+import {CreateTodoButton} from './CreateTodoButton'
+
+const defaultTodos = [
+  {text: 'Cortar cebolla', completed: true},
+  {text: 'Tomar el cursso de intro a React', completed: false},
+  {text: 'Llorar con la llorona', completed: true},
+  {text: 'LALALALAA', completed: false},
+]
+
+function App(){
+  const [todos, setTodos] = useState(defaultTodos)
+  const [searchValue, setSearchValue] = useState('')
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length
+  const totalTodos = todos.length
+
+  let searchedTodos = []
+
+  if(!searchValue.length >= 1){
+    searchedTodos = todos
+  } else{
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase()
+      const searchedText = searchValue.toLowerCase()
+
+      return todoText.includes(searchedText)
+    })
+  }
+
+  return (
+    <React.Fragment>
+      <TodoCounter
+        completed={completedTodos}
+        total={totalTodos}
+      />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+
+      <TodoList>
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton/>
+    </React.Fragment>
+  )
+}
+
+export default App
